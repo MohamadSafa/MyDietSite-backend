2822d487c7865959f32eca79da7828cdb39698fe
Mohamad Safa <mohsafa3@gmail.com>
2024-01-14 00:57:54 +0200

    User role edit from user to customer

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true},
     password: { type: String, required: true},
-    role: { type: String, required: true, default: 'user', enum : ["admin" , "customer"]},
+    role: { type: String, required: true, default: 'customer', enum : ["admin" , "customer"]},
     phoneNumber: { type: String, required: true, unique: true},
 });
 
